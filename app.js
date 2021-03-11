600581f55d2b071e8eb148a08a6eefeedd1a22a4
shashidharreddydakuri <d.shashidhar1996@gmail.com>
2021-03-11 22:50:17 +0530

    hello

--- app.js
+++ app.js
@@ -5,7 +5,9 @@ const app = express();
 
 const PORT = process.env.PORT || 5000;
 
-
+app.get('/', (req,res) => {
+	res.send('hello');
+});
 
 app.listen(PORT, () => {
 	console.log('Server is listening on Port:', PORT);
